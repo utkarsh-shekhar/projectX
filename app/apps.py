4b564d0bedd6d7355607b6d611336fdca19f41f2
rajat garg <rajat garg>
2016-09-04 18:40:36 +0530

    login functionality added

--- app/apps.py
+++ app/apps.py
@@ -3,6 +3,5 @@ from django.apps import AppConfig
 
 class AppConfig(AppConfig):
     name = 'app'
-    #adding the Logout functionality
-    def Logout(request)
-        return render(request, 'apps/index.html')
+    
+ 
